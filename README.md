71864ac1ff0cbf214465ec9cf5cbb440052a8ec5
Joao Micena (JP) <joao.micena@nubank.com.br>
2019-11-26 11:34:16 -0300

    Initial Commit and Problem 01

---
+++ README.md
@@ -0,0 +1,7 @@
+## Scala 99 Problems
+
+The problems are described in http://aperiodic.net/phil/scala/s-99/
+
+Every problem has its own file.
+
+Run with SBT
---
+++ src/main/scala/p01.scala
@@ -0,0 +1,38 @@
+import scala.annotation._
+
+object p01 extends App {
+
+  /*
+      PROBLEM 01
+
+      (*) Find the last element of a list.
+      Example:
+        scala> last(List(1, 1, 2, 3, 5, 8))
+        res0: Int = 8
+
+   */
+
+  val myList: List[Int] = List(1, 1, 2, 3, 5, 8)
+
+  def last[A](l: List[A]): A = {
+    @tailrec
+    def lastHelper(remaning: List[A]): A =
+      if (remaning.tail.isEmpty) remaning.head
+      else lastHelper(remaning.tail)
+
+    lastHelper(l)
+  }
+
+  def lastBuiltin[A](l: List[A]): A = l.last
+
+  def lastWithCase[A](l: List[A]): A = l match {
+    case h :: Nil => h
+    case _ :: tail => lastWithCase(tail)
+    case _ => throw new NoSuchElementException
+  }
+
+  println(last(myList))
+  println(lastBuiltin(myList))
+  println(lastWithCase(myList))
+
+}
