e681184d074e4a106b3bdb944317e4dd01c0d917
Joao Micena (JP) <joao.micena@nubank.com.br>
2019-11-26 11:49:41 -0300

    Problem 02

---
+++ src/main/scala/p02.scala
@@ -0,0 +1,40 @@
+import scala.annotation._
+
+object p02 extends App {
+
+  /*
+    PROBLEM 02
+
+    (*) Find the last but one element of a list.
+    Example:
+      scala> penultimate(List(1, 1, 2, 3, 5, 8))
+      res0: Int = 5
+ */
+
+  val myList: List[Int] = List(1, 1, 2, 3, 5, 8)
+
+  @tailrec
+  def penultimate[A](l: List[A]): A = l match {
+    case h :: _ :: Nil => h
+    case _ :: tail => penultimate(tail)
+    case _ => throw new NoSuchElementException
+  }
+
+  def penultimateWithoutCase[A](l: List[A]): A = {
+    @tailrec
+    def penultimateHelper(remaining: List[A]): A = {
+      if (remaining.length < 2) throw new NoSuchElementException
+      else if (remaining.length == 2) remaining.head
+      else penultimateHelper(remaining.tail)
+    }
+
+    penultimateHelper(l)
+  }
+
+  def penultimateBuiltin[A](l: List[A]): A = l.init.last
+
+  println(penultimate(myList))
+  println(penultimateWithoutCase(myList))
+  println(penultimateBuiltin(myList))
+
+}
