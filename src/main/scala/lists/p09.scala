169cd46370c92affdbfac1346da8b0f428c4b449
Joao Micena (JP) <joao.micena@nubank.com.br>
2019-12-10 16:48:18 -0300

    Problem 09

---
+++ src/main/scala/lists/p09.scala
@@ -0,0 +1,34 @@
+package lists
+
+object p09 extends App {
+
+  /*
+    PROBLEM 09
+
+     (**) Pack consecutive duplicates of list elements into sublists.
+      If a list contains repeated elements they should be placed in separate sublists.
+      Example:
+        scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+        res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+
+  */
+
+  val myList = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e")
+
+  def pack[A](l: List[A]): List[List[A]] = l.distinct.foldLeft(List.empty[List[A]]) { (ls, e) =>
+    ls :+ l.filter(x => x == e)
+  }
+
+  def packRecursive[A](l: List[A]): List[List[A]] = {
+    if (l.isEmpty) List(List())
+    else {
+      val (packed, next) = l span { _ == l.head }
+      if (next == Nil) List(packed)
+      else packed :: packRecursive(next)
+    }
+  }
+
+  println(pack(myList))
+  println(packRecursive(myList))
+
+}
